275a3be144b02c3b3e9bfc4120dda176e43f0620
ZacharyWin <2816813070@qq.com>
2022-03-03 15:50:52 +0800

    feat: add external infer type

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type ExtractProperties<T> = T extends (...args: any) => any ? T : never
+
+type FuncName<T> = {
+    [P in keyof T]: T[P] extends Function ? P : never
+}[keyof T]
+
+type PropertyName<T> = {
+    [P in keyof T]: T[P] extends Function ? never : P
+}[keyof T]
+
+export type ClassFunc<M> = { [T in FuncName<M>]?: M[T] }
+
+export type ClassProperty<M> = { [T in PropertyName<M>]?: M[T] }
