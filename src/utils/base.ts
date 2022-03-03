484cd3e5d167bdc8de2590d55b924e1bf2cf2d02
ZacharyWin <2816813070@qq.com>
2022-03-03 15:51:17 +0800

    feat: add utils

---
+++ src/utils/base.ts
@@ -0,0 +1,16 @@
+import { ClassProperty } from '@/types'
+
+export function defineProperties<T>(
+    self: ThisType<T>,
+    props: ClassProperty<T>
+) {
+    for (let key in self) {
+        if (key in props) {
+            self[key] = props[key]
+        }
+    }
+}
+
+export function modelFromJson<T>(json: string): T {
+    return JSON.parse(json) as T
+}
