496611adcf0352d1a51d30db4b96a34705341ca7
ZacharyWin <2816813070@qq.com>
2022-03-04 21:56:28 +0800

    fix: `ClassPropertyWithExcludeExtend`

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-export type ExtractProperties<T> = T extends (...args: any) => any ? T : never
-
 type FuncName<T> = {
     [P in keyof T]: T[P] extends Function ? P : never
 }[keyof T]
@@ -12,7 +10,14 @@ export type ClassFunc<M> = { [T in FuncName<M>]?: M[T] }
 
 export type ClassProperty<M> = { [T in PropertyName<M>]?: M[T] }
 
+// exclude properties U from T, and get the rest properties name
+type Exclude<T, U> = {
+    [P in keyof T]: P extends U ? never : P
+}[keyof T]
+
 /**
  * Exclude from M those types that are assignable to E
  */
-export type ClassPropertyWithExcludeExtend<M, E> = ClassProperty<Exclude<M, E>>
+export type ClassPropertyWithExcludeExtend<M, E> = ClassProperty<{
+    [T in Exclude<M, keyof E>]?: M[T]
+}>
