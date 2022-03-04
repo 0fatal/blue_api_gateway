516ee98d879911d8a5f7fffb87bc1d0a11a0e472
ZacharyWin <2816813070@qq.com>
2022-03-04 19:23:45 +0800

    update: config

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,9 +1,22 @@
-import { MidwayConfig } from '@midwayjs/core';
+import { MidwayConfig } from '@midwayjs/core'
 
 export default {
-  // use for cookie sign key, should change to your own and keep security
-  keys: '1646199237666_8403',
-  koa: {
-    port: 7001,
-  },
-} as MidwayConfig;
+    // use for cookie sign key, should change to your own and keep security
+    keys: '1646199237666_8403',
+    koa: {
+        port: 7001,
+    },
+    authService: {
+        TOKEN_EXPIRE: 86400 * 30, // 60天
+        AUTH_FLAG: 'GatewayAuth',
+    },
+    orm: {
+        synchronize: true,
+    },
+    cors: {
+        credentials: false,
+        allowHeaders: '*',
+        allowMethods: '*',
+        origin: '*',
+    },
+} as MidwayConfig
