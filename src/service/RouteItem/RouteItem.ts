f79559711bdc24d37e02904814ab50acb96df18d
ZacharyWin <2816813070@qq.com>
2022-03-04 21:55:07 +0800

    update

--- src/service/RouteItem/RouteItem.ts
+++ src/service/RouteItem/RouteItem.ts
@@ -1,4 +1,5 @@
 import { InjectEntityModel } from '@midwayjs/orm'
+import superagent = require('superagent')
 import { Repository } from 'typeorm'
 import { RouteItem } from '../../model/routerItem/routeItem'
 
@@ -6,9 +7,10 @@ export class RouteItemService {
     @InjectEntityModel(RouteItem)
     routeItemModel: Repository<RouteItem>
 
-    async getRouteItem(location: string, method: string) {
+    async getRouteItem(father, location: string, method: string) {
         return await this.routeItemModel.findOne({
             where: {
+                father: father,
                 method: method,
                 location: location,
             },
