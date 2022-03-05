8732c7dfc7ab5271e4b801c8f4c981f59f6a4952
ZacharyWin <2816813070@qq.com>
2022-03-05 09:34:32 +0800

    chore: change tsconfig

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -1,18 +1,120 @@
-import { Inject, Controller, Get, Query } from '@midwayjs/decorator';
-import { Context } from '@midwayjs/koa';
-import { UserService } from '../service/user.service';
+import { Response, ServiceResp } from '@/dto/response'
+import { CustomException } from '@/exception'
+import { RouteItem } from '@/model/routerItem/routeItem'
+import { RouteItemService } from '@/service/RouteItem/RouteItem'
+import { Inject, Controller, All } from '@midwayjs/decorator'
+import { Context } from '@midwayjs/koa'
+import request = require('superagent')
 
-@Controller('/api')
+@Controller('/v1')
 export class APIController {
-  @Inject()
-  ctx: Context;
+    @Inject()
+    ctx: Context
 
-  @Inject()
-  userService: UserService;
+    @Inject()
+    routeItemService: RouteItemService
 
-  @Get('/get_user')
-  async getUser(@Query('uid') uid) {
-    const user = await this.userService.getUser({ uid });
-    return { success: true, message: 'OK', data: user };
-  }
+    @All('/*')
+    async forwardRequest() {
+        const path = this.ctx.path
+        const pathSplit = path.split('/', 3)
+        const method = this.ctx.method
+
+        if (pathSplit.length !== 3) {
+            throw CustomException.Make('NOT_FOUND')
+        }
+        const father = pathSplit[1]
+        const location = pathSplit[2]
+
+        const rt = await this.routeItemService.getRouteItem(
+            father,
+            location,
+            method
+        )
+
+        if (!rt) {
+            throw CustomException.Make('NOT_FOUND')
+        }
+
+        rt.checkScope(this.ctx)
+
+        const req = rt.makeNewRemoteRequest()
+
+        const setRequestHeader = (
+            ctx: Context,
+            req: request.Request,
+            rt: RouteItem
+        ) => {
+            for (const headerName of rt.DirectThroughRequestHeaders) {
+                const headerValue = ctx.header[headerName]
+                req.set(headerName, headerValue as string)
+            }
+        }
+
+        const copyHeaderFromResp = (
+            ctx: Context,
+            resp: request.Response,
+            rt: RouteItem
+        ) => {
+            for (const headerName of rt.DirectThroughResponseHeaders) {
+                const headerValue = resp.get(headerName)
+                if (headerName) {
+                    ctx.header[headerName] = headerValue
+                }
+            }
+        }
+
+        const setBody = (ctx: Context, req: request.Request, rt: RouteItem) => {
+            const body = ctx.request.body
+            if (body) {
+                req.send(body)
+            }
+        }
+
+        setRequestHeader(this.ctx, req, rt)
+        setBody(this.ctx, req, rt)
+        const resp = await req
+        if (resp.error) {
+            throw CustomException.Make('SERVER_ERROR', resp.error.text)
+        }
+
+        const respBody = resp.body as ServiceResp
+        if (resp.statusCode !== 200 && respBody.data) {
+            const r = Response.MakeJSONErrorWithData(
+                resp.statusCode,
+                respBody.code,
+                respBody.msg,
+                respBody.data
+            )
+            throw new CustomException({
+                httpCode: resp.statusCode,
+                payload: r,
+            })
+        }
+
+        if (resp.statusCode !== 200 && !respBody.data) {
+            const r = Response.MakeJSONError(
+                resp.statusCode,
+                respBody.code,
+                respBody.msg
+            )
+            throw new CustomException({
+                httpCode: resp.statusCode,
+                payload: r,
+            })
+        }
+
+        if (rt.needRedirect && respBody.redirect) {
+            const r = Response.MakeRedirect(respBody.redirect)
+            throw new CustomException({
+                httpCode: resp.statusCode,
+                payload: r,
+            })
+        }
+
+        const r = Response.MakeJSONSuccess(respBody.data)
+        copyHeaderFromResp(this.ctx, resp, rt)
+
+        return r.JSON
+    }
 }
