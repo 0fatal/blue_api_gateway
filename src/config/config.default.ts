7489f703adaf71d88e9f0864c707de5719de9528
ZacharyWin <2816813070@qq.com>
2022-03-05 11:41:14 +0800

    feat(config): add orm config

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -12,6 +12,12 @@ export default {
     },
     orm: {
         synchronize: true,
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'blue_api_gateway',
     },
     cors: {
         credentials: false,
